12c3e34636dbcc7733ea2bef9670d66da39e8eb7
Lucas Mansoldo <lucasmansoldo@Mac-mini-de-Lucas.local>
2026-06-12 09:41:46 -0300

    feat(ui): export all primitive components from barrel index

--- packages/ui/src/lib/index.ts
+++ packages/ui/src/lib/index.ts
@@ -1,2 +1,8 @@
-// Components and registry will be exported here in Plan 1
-export {}
+// Primitives (Plan 1a)
+export { default as Button } from './components/Button/index.js';
+export { default as Badge } from './components/Badge/index.js';
+export { default as Input } from './components/Input/index.js';
+export { default as Avatar } from './components/Avatar/index.js';
+export { default as Spinner } from './components/Spinner/index.js';
+export { default as Toast } from './components/Toast/index.js';
+export { default as FilterBar } from './components/FilterBar/index.js';
